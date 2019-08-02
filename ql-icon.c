c94053546505cb91983b91eb78346022d29cbaf9
Ingve Vormestrand <ingvev@gmail.com>
2019-08-02 23:41:27 +0200

    Experimental stdout option support

--- ql-icon.c
+++ ql-icon.c
@@ -10,6 +10,10 @@
 #define ERROR_IMAGE_TYPE_NOT_SUPPORTED 2
 #define ERROR_INVALID_PARAMETER 3
 #define ERROR_MISSING_REQUIRED_PARAMETER 4
+#define ERROR_GENERATING_IMAGE 5
+#define ERROR_CREATING_IMAGE_DESTINATION 6
+#define ERROR_FINALIZING_IMAGE 7
+#define ERROR_WRITING_OUTPUT 8
 #define ERROR_INTERNAL_ERROR 127
 
 void usage(const char *binary_name) {
@@ -48,22 +52,11 @@ bool is_type_supported(const char *type) {
     return is_supported;
 }
 
-bool save_icon(const char *filename, const char *icon_name, const char *type, unsigned int width, unsigned int height, float compression_quality) {
-    int res = 1;
-    CFStringRef type_str = NULL;
+CGImageRef image_for_file(const char *filename, unsigned int width, unsigned int height) {
+    CGImageRef image = NULL;
     CFStringRef file_str = NULL;
     CFURLRef file_url = NULL;
-    CFStringRef icon_str = NULL;
-    CFURLRef icon_url = NULL;
-    CGImageRef image = NULL;
-    CGImageDestinationRef icon = NULL;
-    CFDictionaryRef properties  = NULL;
-    CFStringRef property_names[1];
-    CFTypeRef property_values[1];
-    property_names[0] = kCGImageDestinationLossyCompressionQuality;
-    property_values[0] = CFNumberCreate(NULL, kCFNumberFloatType, &compression_quality);
-    properties = CFDictionaryCreate(NULL, (const void **)property_names, (const void **)property_values, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
-
+    
     CFDictionaryRef options = NULL;
     CFStringRef options_names[1];
     CFTypeRef options_values[1];
@@ -72,25 +65,79 @@ bool save_icon(const char *filename, const char *icon_name, const char *type, un
     options = CFDictionaryCreate(NULL, (const void **)options_names, (const void **)options_values, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
 
     CGSize size = CGSizeMake(width, height);
-    type_str = CFStringCreateWithCString(NULL, type, kCFStringEncodingUTF8);
+
     file_str = CFStringCreateWithCString(NULL, filename, kCFStringEncodingUTF8);
     file_url = CFURLCreateWithFileSystemPath(NULL, file_str, kCFURLPOSIXPathStyle, 0);
-    icon_str = CFStringCreateWithCString(NULL, icon_name, kCFStringEncodingUTF8);
-    icon_url = CFURLCreateWithFileSystemPath(NULL, icon_str, kCFURLPOSIXPathStyle, 0);
+
     image = QLThumbnailImageCreate(kCFAllocatorDefault, file_url, size, options);
-    if (image) {
-        icon = CGImageDestinationCreateWithURL(icon_url, type_str, 1, NULL);
-        if (icon) {
-            CGImageDestinationAddImage(icon, image, properties);
-            res = CGImageDestinationFinalize(icon);
-        }
-    }
-    if (property_values[0]) CFRelease(property_values[0]);
-    if (properties) CFRelease(properties);
+
     if (options) CFRelease(options);
-    if (type_str) CFRelease(type_str);
     if (file_str) CFRelease(file_str);
     if (file_url) CFRelease(file_url);
+
+    return image;
+}
+
+int save_icon(const char *filename, const char *icon_name, const char *type, unsigned int width, unsigned int height, float compression_quality) {
+    int res = 0;
+    CFStringRef type_str = NULL;
+    CFStringRef icon_str = NULL;
+    CFURLRef icon_url = NULL;
+    CGImageRef image = NULL;
+    CGImageDestinationRef icon = NULL;
+    CFDictionaryRef properties  = NULL;
+    CFStringRef property_names[1];
+    CFTypeRef property_values[1];
+
+    property_names[0] = kCGImageDestinationLossyCompressionQuality;
+    property_values[0] = CFNumberCreate(NULL, kCFNumberFloatType, &compression_quality);
+    properties = CFDictionaryCreate(NULL, (const void **)property_names, (const void **)property_values, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+
+    type_str = CFStringCreateWithCString(NULL, type, kCFStringEncodingUTF8);
+    
+    icon_str = CFStringCreateWithCString(NULL, icon_name, kCFStringEncodingUTF8);
+    icon_url = CFURLCreateWithFileSystemPath(NULL, icon_str, kCFURLPOSIXPathStyle, 0);
+    
+    image = image_for_file(filename, width, height);
+    if (!image) {
+        res = ERROR_GENERATING_IMAGE;
+        goto cleanup;
+    }
+
+    CFMutableDataRef data = NULL;
+
+    if (strlen(icon_name) == 0) {
+        data = CFDataCreateMutable(kCFAllocatorDefault, 0);
+        icon = CGImageDestinationCreateWithData(data, type_str, 1, NULL);
+    } else {
+        icon = CGImageDestinationCreateWithURL(icon_url, type_str, 1, NULL);
+    }
+
+    if (!icon) {
+        res = ERROR_CREATING_IMAGE_DESTINATION;
+        goto cleanup;
+    }
+
+    CGImageDestinationAddImage(icon, image, properties);
+    if (!CGImageDestinationFinalize(icon)) {
+        res = ERROR_FINALIZING_IMAGE;
+        goto cleanup;
+    }
+
+    if (strlen(icon_name) == 0) {
+        CFIndex len = CFDataGetLength(data);
+        long written = fwrite(CFDataGetBytePtr(data), 1, len, stdout); 
+        if (written < len) {
+            res = ERROR_WRITING_OUTPUT;
+            goto cleanup;
+        }
+        if (data) CFRelease(data);
+    }
+
+cleanup:
+    if (property_values[0]) CFRelease(property_values[0]);
+    if (properties) CFRelease(properties);
+    if (type_str) CFRelease(type_str);
     if (icon_str) CFRelease(icon_str);
     if (icon_url) CFRelease(icon_url);
     if (image) CFRelease(image);
@@ -176,15 +223,18 @@ int main(int argc, char** argv) {
     if (!input)
         fatal("input file name must be specified", ERROR_MISSING_REQUIRED_PARAMETER);
     if (!output)
-        fatal("output file name must be specified", ERROR_MISSING_REQUIRED_PARAMETER);
+        output = "";
     if (verbose)
         fprintf(stderr, "width: %d, height: %d, input: %s, output: %s, type: %s, quality: %.2f\n", width, height, input, output, type, quality);
 
-    if (!save_icon(input, output, type, width, height, quality))
+    int res = save_icon(input, output, type, width, height, quality);
+    if (res < 0) {
         fatal("failed to generate icon", ERROR_GENERATING_ICON);
+    }
 
     free(input);
-    free(output);
+    if (strlen(output) > 0)
+        free(output);
     if (type_specified)
         free(type);
     
